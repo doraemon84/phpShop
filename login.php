9c1c2954d757d49450a021845d4acfd9f04cd787
Daniel <dts@baltic-online.de>
2013-08-16 08:52:13 +0200

    Added login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+@
+$username = S_POST['username'];
+$password = $_POST['password'];
+
+if ($username&&password)
+
+{
+	$connect = mysql_connect("localhost",admin,"test123") or die ("Couldn't connect to the database!");
+	mysql_select_db("phpShop") or die("Couldnt find database");	
+}
+
+else 
+	die("Please enter a username and a password");
+
+?>
