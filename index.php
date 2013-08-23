8c376f204f9b250f7b464a87553ebfa21d41399c
dts <dts@ubuntu.(none)>
2013-08-22 23:07:31 -0700

    changed html to php

--- index.php
+++ index.php
@@ -1,8 +1,225 @@
-<html>
-	
-		<form action='login.php' method='POST'>
-				Username: <input type='text' name='username'>
-				Password : <input type='password' name='password'>
-				<input type='submit' value='Log in'>
-		</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Nippon Shoppers" content="">
+    <meta name="Daniel Takahashi-Schwarz" content="">
+    <link rel="shortcut icon" href="bootstrap/icon/favicon.png">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/carousel.css" rel="stylesheet">
+   
+    <style>
+    
+      .black
+      	{
+		color: #343434;
+		}
+		
+		.test
+		{
+		color:#fffff;
+		font-size: 20px;
+		text-align: center;
+		}
+		
+     </style>
+     
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">NihonFood.de</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">Über uns</a></li>
+            <li><a href="#contact">Kontakt</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Lebensmittel</a></li>
+                <li><a href="#">Küche</a></li>
+                <li><a href="#">Kleidung</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Nippon</a></li>
+                <li><a href="#">FAQ</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-right" action="login.php" method="POST">
+            <div class="form-group">
+              <input type="text" name="username" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+        	 <input type="password" name="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" value="Log in" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="bootstrap/img/asakusa.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Willkommen auf NihonFood.de</h1>
+              <p>Der größte japanische Online Shop Deutschlands</p>
+              <p><a class="btn btn-large btn-primary" href="#">Register</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="bootstrap/img/matcha.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Frisch und Schnell </h1>
+              <p>Japanische Spezialitäten zu jeder Zeit bestellen</p>
+              <p><a class="btn btn-large btn-primary" href="#">Shop</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="bootstrap/img/maiko.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <H1>Welcome to Japan.</H1>
+              <p class="black"></p>
+              <p><a class="btn btn-large btn-primary" href="#">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
+
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-square" src="bootstrap/img/japankleidung.jpg" data-src="holder.js/140x140" alt="">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+    
+        </div><!-- /.col-lg-4 -->
+        
+         <div class="col-lg-4">
+          <img class="img-circle" src="bootstrap/img/hashi.jpg" alt="">
+          <h2>Küche</h2>
+          <p>In unserem ausgewählten Küchensortiment finden Sie diverses japanisches Küchenequipment.</p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+    
+        </div><!-- /.col-lg-4 -->
+    
+        
+        <div class="col-lg-4">
+          <img class="img-circle" src="data:image/png;base64," data-src="holder.js/140x140" alt="Generic placeholder image">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Develop your product together. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="bootstrap/img/pic_1.jpg" alt="">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="data:image/png;base64," data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="data:image/png;base64," data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/holder.js"></script>
+    
+  </body>
 </html>
